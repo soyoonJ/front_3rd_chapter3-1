1ed1a38b3408300f1c96ffda2371709b279a0437
soyoonJ <sytkfkd96@gmail.com>
2024-11-03 04:27:15 +0900

    test: easy.notificationUtils.spec.ts getUpcomingEvents describe 내 events 공통 사용

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -2,41 +2,27 @@ import { Event } from '../../types';
 import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
 
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
-    const events: Event[] = [
-      {
-        id: '1',
-        title: '이벤트 1',
-        date: '2024-11-03',
-        startTime: '09:00',
-        endTime: '10:00',
-        description: '',
-        location: '',
-        category: '',
-        repeat: { type: 'none', interval: 0 },
-        notificationTime: 60,
-      },
-    ];
+  const events: Event[] = [
+    {
+      id: '1',
+      title: '이벤트 1',
+      date: '2024-11-03',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '',
+      location: '',
+      category: '',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 60,
+    },
+  ];
 
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
     const upcomingEvents = getUpcomingEvents(events, new Date(2024, 10, 3, 8), []);
     expect(upcomingEvents).toEqual([events[0]]);
   });
 
   it('이미 알림이 간 이벤트는 제외한다', () => {
-    const events: Event[] = [
-      {
-        id: '1',
-        title: '이벤트 1',
-        date: '2024-11-03',
-        startTime: '09:00',
-        endTime: '10:00',
-        description: '',
-        location: '',
-        category: '',
-        repeat: { type: 'none', interval: 0 },
-        notificationTime: 60,
-      },
-    ];
     const notifiedEvents = ['1'];
 
     const upcomingEvents = getUpcomingEvents(events, new Date(2024, 10, 3, 8), notifiedEvents);
@@ -44,41 +30,11 @@ describe('getUpcomingEvents', () => {
   });
 
   it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
-    const events: Event[] = [
-      {
-        id: '1',
-        title: '이벤트 1',
-        date: '2024-11-03',
-        startTime: '09:00',
-        endTime: '10:00',
-        description: '',
-        location: '',
-        category: '',
-        repeat: { type: 'none', interval: 0 },
-        notificationTime: 60,
-      },
-    ];
-
     const upcomingEvents = getUpcomingEvents(events, new Date(2024, 10, 3, 7), []);
     expect(upcomingEvents).toEqual([]);
   });
 
   it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
-    const events: Event[] = [
-      {
-        id: '1',
-        title: '이벤트 1',
-        date: '2024-11-03',
-        startTime: '09:00',
-        endTime: '10:00',
-        description: '',
-        location: '',
-        category: '',
-        repeat: { type: 'none', interval: 0 },
-        notificationTime: 60,
-      },
-    ];
-
     const upcomingEvents = getUpcomingEvents(events, new Date(2024, 10, 3, 10), []);
     expect(upcomingEvents).toEqual([]);
   });
