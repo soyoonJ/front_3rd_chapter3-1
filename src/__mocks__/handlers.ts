6dc646cef64eaa2baf31975f5e708c1cac95722a
soyoonJ <sytkfkd96@gmail.com>
2024-11-03 20:01:03 +0900

    test: msw handlers 추가

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -4,13 +4,29 @@ import { Event } from '../types';
 import { events } from './response/events.json' assert { type: 'json' };
 
 // ! HARD
-// ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
+// ! 각 응답에 대한 MSW 핸들러핸들러핸들러핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
+
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const body = await request.json();
 
-  http.put('/api/events/:id', async () => {}),
+    const newEvent = { ...(body as Event), id: String(events.length + 1) };
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+    return HttpResponse.json(newEvent, { status: 201 });
+  }),
+
+  // TODO: update/delete params 활용?
+  http.put('/api/events/:id', async ({ request }) => {
+    const updates = await request.json();
+
+    return HttpResponse.json(updates);
+  }),
+
+  http.delete('/api/events/:id', ({ params }) => {
+    return HttpResponse.json(null, { status: 204 });
+  }),
 ];
